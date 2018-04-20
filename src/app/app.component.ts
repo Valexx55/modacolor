7007e917f034df4240662d71fbe57cf24c5d5340
Val <valexx55@gmail.com>
2018-04-20 12:23:50 +0200

    subida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+
+    private ruta_foto; //= 'assests/bodydn.jpg',
+    private colores; //= ['red', 'black'],
+    private color_selec;// = 'black'
+
+  constructor() {
+    this.ruta_foto = 'assets/bodydn.jpg';
+    this.colores = ['red', 'black'];
+    this.color_selec = 'black';
+   }
+
+   cambio(color_tocado : string) : void
+   {
+     console.log ("Toco color "+color_tocado);
+     color_tocado === 'red' ? (this.ruta_foto='assets/bodydr.jpg') : ((this.ruta_foto='assets/bodydn.jpg'))
+   }
+
+   ngDoCheck ()
+   {
+     console.log ("NG DOCHECK invocado");
+   }
 }
+
